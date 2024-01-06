f0fd43ecc849aaa0df4039f3a832786d43021d11
y1j2x34 <y1j2x34@qq.com>
2024-01-06 23:02:26 +0800

    wip: add ClassDecoratorHandler type

--- packages/solidium/src/core/DecoratorHandler.ts
+++ packages/solidium/src/core/DecoratorHandler.ts
@@ -3,6 +3,10 @@ import { ClassMetadataReader, MemberKey, Newable } from '@vgerbot/ioc';
 export const IS_MEMBER_DECORATOR_HANDLER = Symbol(
     'solidium-is-member-decorator-handler'
 );
+export const IS_CLASS_DECORATOR_HANDLER = Symbol(
+    'solidium-is-class-decorator-handler'
+);
+
 export interface MemberDecoratorHandler {
     [IS_MEMBER_DECORATOR_HANDLER]: true;
     beforeInstantiation?: <T>(
@@ -15,4 +19,16 @@ export interface MemberDecoratorHandler {
         member: MemberKey,
         metadata: ClassMetadataReader<T>
     ) => void;
+}
+
+export interface ClassDecoratorHandler {
+    [IS_CLASS_DECORATOR_HANDLER]: true;
+    beforeInstantiation?: <T>(
+        constructor: Newable<T>,
+        metadata: ClassMetadataReader<T>
+    ) => void;
+    afterInstantiation?: <T>(
+        instance: T,
+        metadata: ClassMetadataReader<T>
+    ) => T;
 }
