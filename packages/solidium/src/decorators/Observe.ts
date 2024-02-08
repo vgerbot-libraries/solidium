0771403ace6c57bacc202e211887909328d8875e
y1j2x34 <y1j2x34@qq.com>
2024-02-08 10:14:14 +0800

    wip: @Observer supports defer

--- packages/solidium/src/decorators/Observe.ts
+++ packages/solidium/src/decorators/Observe.ts
@@ -3,38 +3,46 @@ import {
     MemberDecoratorProcessor,
     IS_MEMBER_DECORATOR_PROCESSOR
 } from '../core/DecoratorProcessor';
-import { createEffect, onCleanup } from 'solid-js';
+import { AccessorArray, createEffect, on, onCleanup } from 'solid-js';
 import { clean, store } from '../common/store-result';
 
 export const OBSERVE_PROPERTY_MARK_KEY = Symbol('solidium_observed_property');
-export type ObserveOptions = {
-    collectOnce?: boolean;
-    schedule?:
-        | undefined
-        | {
-              mode: 'throttle';
-              trailing?: boolean;
-              leading?: boolean;
-              wait?: number;
-          }
-        | {
-              mode: 'debounce';
-              trailing?: boolean;
-              leading?: boolean;
-              wait?: number;
-              maxWait?: number;
-          };
-};
+export type ObserveOptions =
+    | {}
+    | {
+          deps: AccessorArray<unknown>;
+          defer?: boolean;
+      }
+    | {
+          schedule:
+              | {
+                    mode: 'throttle';
+                    trailing?: boolean;
+                    leading?: boolean;
+                    wait?: number;
+                }
+              | {
+                    mode: 'debounce';
+                    trailing?: boolean;
+                    leading?: boolean;
+                    wait?: number;
+                    maxWait?: number;
+                };
+      };
 interface ObserverableObject {
     [key: MemberKey]: () => unknown;
 }
-
-export const Observe = (options: ObserveOptions = { collectOnce: false }) =>
+/**
+ *
+ * @param options optional
+ * @returns an method decorator
+ */
+export const Observe = (options: ObserveOptions = {}) =>
     Mark(OBSERVE_PROPERTY_MARK_KEY, {
         [IS_MEMBER_DECORATOR_PROCESSOR]: true,
         afterInstantiation(instance, methodName) {
             // TODO: supports scheduling
-            createEffect(() => {
+            const fn = () => {
                 const ret = (instance as ObserverableObject)[methodName].call(
                     instance
                 );
@@ -42,7 +50,16 @@ export const Observe = (options: ObserveOptions = { collectOnce: false }) =>
                 onCleanup(() => {
                     clean(instance as Object, methodName);
                 });
-            });
+            };
+            if ('deps' in options) {
+                createEffect(
+                    on(options.deps, fn, {
+                        defer: options.defer
+                    })
+                );
+            } else {
+                createEffect(fn);
+            }
             return instance;
         }
     } as MemberDecoratorProcessor) as MethodDecorator;
