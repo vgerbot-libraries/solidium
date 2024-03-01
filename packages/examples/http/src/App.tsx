ad3f8719fb3f8fc9527041b3d1f58e86d3289828
y1j2x34 <y1j2x34@qq.com>
2024-03-01 18:00:13 +0800

    chore: data resource example

--- packages/examples/http/src/App.tsx
+++ packages/examples/http/src/App.tsx
@@ -1,6 +1,21 @@
 import { Solidium } from '@vgerbot/solidium';
 import { HttpClient } from '@vgerbot/solidium-http';
 import { TodoList } from './Todolist';
+import { HttpInterceptor } from 'packages/http/lib/types/HttpInterceptor';
+import { HttpRequest } from 'packages/http/lib/types/HttpRequest';
+import { HttpResponse } from 'packages/http/lib/types/HttpResponse';
+class AddRandomParamInterceptor implements HttpInterceptor {
+    name = 'ADD-RANDOM-PARAM';
+    intercept(
+        request: HttpRequest,
+        next: (request: HttpRequest) => Promise<HttpResponse>
+    ): Promise<HttpResponse> {
+        if (Math.random() > 0.5) {
+            request.url.searchParams.append('_random', Math.random() + '');
+        }
+        return next(request);
+    }
+}
 
 export function App() {
     return (
@@ -8,7 +23,8 @@ export function App() {
             autoRegisterClasses={[
                 HttpClient.configure({
                     baseUrl:
-                        'https://65e05c45d3db23f7624913ab.mockapi.io/api/v1/'
+                        'https://65e05c45d3db23f7624913ab.mockapi.io/api/v1/',
+                    interceptors: [new AddRandomParamInterceptor()]
                 })
             ]}
         >
--- packages/examples/http/src/Todolist.tsx
+++ packages/examples/http/src/Todolist.tsx
@@ -1,19 +1,46 @@
-import { useResource } from '@vgerbot/solidium-http';
+import { useJSON } from '@vgerbot/solidium-http';
+import { For, createEffect } from 'solid-js';
 import { Show } from 'solid-js/web';
 
 export function TodoList() {
-    const resource = useResource({
+    const resource = useJSON<
+        Array<{
+            createdAt: string;
+            name: string;
+            avatar: string;
+            id: string;
+        }>
+    >({
         url: 'tasks'
+    });
+    createEffect(() => {
+        console.log(resource.data);
     });
     return (
         <div>
             Tasks:
             <Show when={resource.pending}>Loading....</Show>
             <Show when={resource.success}>
-                {(() => {
-                    console.log(resource);
-                    return '';
-                })()}
+                <ul>
+                    <For each={resource.data || []}>
+                        {it => {
+                            return (
+                                <li>
+                                    <img
+                                        src={it.avatar}
+                                        width={32}
+                                        height={32}
+                                        alt={'Avatar of ' + it.name}
+                                    />
+                                    <dl>
+                                        <dt>{it.name}</dt>
+                                        <dd>{it.createdAt}</dd>
+                                    </dl>
+                                </li>
+                            );
+                        }}
+                    </For>
+                </ul>
             </Show>
             <Show when={resource.failure}>Failed!</Show>
         </div>
