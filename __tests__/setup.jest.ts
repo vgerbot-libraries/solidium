f3a29faaf58212fbc0933177c6e59132e30a2daf
y1j2x34 <y1j2x34@qq.com>
2024-07-15 20:29:33 +0800

    test: update configuration

---
+++ __tests__/setup.jest.ts
@@ -0,0 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { TextDecoder, TextEncoder } from 'util';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+globalThis.TextDecoder = TextDecoder;
+globalThis.TextEncoder = TextEncoder;
