bf663eee8f6eeda9865da64c749d0f4f6f549467
y1j2x34 <y1j2x34@qq.com>
2025-02-16 01:50:28 +0800

    chore: upgrade eslint and configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,59 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier';
+import globals from 'globals';
+import path from 'path';
+
+const __dirname = path.resolve('.');
+
+export default tseslint.config(
+    tseslint.configs.strict,
+    eslint.configs.recommended,
+    // 自定义配置需要放在strict配置之前以覆盖其规则
+    [
+        {
+            files: ['**/*.ts', '**/*.tsx'],  // 使用**匹配嵌套目录
+            rules: {
+                'no-console': 'off',
+                'no-bitwise': 'off',
+                quotes: ['error', 'single'],
+                'max-len': ['error', 120],
+                'arrow-parens': 'off',
+                '@typescript-eslint/no-dynamic-delete': 'off'
+            },
+            languageOptions: {
+                parserOptions: {
+                    project: [__dirname + '/tsconfig.json', __dirname + '/tsconfig.test.json']
+                },    
+            }
+        },
+        {
+            files: ['scripts/*.mjs'],
+            languageOptions: {
+                globals: {
+                    ...globals.node,
+                    '$': 'readonly',
+                    'fs': 'readonly',
+                    'path': 'readonly',
+                    'argv': 'readonly'
+                }
+            },
+            rules: {
+                "@typescript-eslint/no-require-imports": 'off'
+            }
+        },
+        {
+            ignores: [
+                '**/lib',
+                '.github',
+                '.husky'
+            ],
+            plugins: {
+                prettier: prettier
+            },
+            rules: {
+                'prettier/prettier': 'error'
+            }
+        }
+    ]
+)
--- scripts/test.mjs
+++ scripts/test.mjs
@@ -1,4 +1,4 @@
-import { pkg, cwd } from './base.mjs';
+import { pkg } from './base.mjs';
 
 if (pkg.scripts && pkg.scripts.test) {
     await $`cross-env NODE_ENV=test npm t -- --passWithNoTests`;
