3e301696a378e93999ba19039b4fe922f78722ba
y1j2x34 <y1j2x34@qq.com>
2025-02-25 23:51:31 +0800

    chore: update example

--- packages/examples/http/src/App.tsx
+++ packages/examples/http/src/App.tsx
@@ -1,5 +1,6 @@
 import { Solidium, useService } from '@vgerbot/solidium';
 import { ObjectsEndpoint } from './ObjectEndpoint';
+import { createEffect, Show } from 'solid-js';
 
 export function App() {
     return (
@@ -14,11 +15,28 @@ function Objects() {
 
     const listRes = endpoint.listAll();
 
+    createEffect(() => {
+        console.log('pending', listRes.pending);
+        console.log('success', listRes.success);
+        console.log('failure', listRes.failure);
+        console.log('abortde', listRes.aborted);
+        console.log('========');
+    });
+
     return (
-        <ul>
-            {listRes.data?.map(it => {
-                return <li>{it.name}</li>;
-            })}
-        </ul>
+        <>
+            <Show when={listRes.pending}>
+                <h1>loading.....</h1>
+            </Show>
+            <Show when={listRes.success}>
+                <ul>
+                    {listRes.data?.map(it => {
+                        return <li>{it.name}</li>;
+                    })}
+                </ul>
+            </Show>
+            <Show when={listRes.failure}>Error: {listRes.error + ''}</Show>
+            <Show when={listRes.aborted}>Aborted</Show>
+        </>
     );
 }
