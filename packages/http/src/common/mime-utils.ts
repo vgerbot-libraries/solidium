892f9809381ecf8389588a55f3132fec57c36056
y1j2x34 <y1j2x34@qq.com>
2025-03-09 23:25:25 +0800

    refactor: Expand supported JSON and text MIME types detection

--- packages/http/src/common/mime-utils.ts
+++ packages/http/src/common/mime-utils.ts
@@ -1,9 +1,26 @@
+const JSON_CONTENT_TYPES = [
+    'application/json',
+    'application/json-patch+json',
+    'application/vnd.api+json',
+    'application/geo+json',
+    'application/schema+json'
+];
+
 export function isJSON(contentType?: string): boolean {
-    return !!contentType && contentType.includes('application/json');
+    return (
+        !!contentType &&
+        JSON_CONTENT_TYPES.some(type => contentType.includes(type))
+    );
 }
 export function isText(contentType?: string): boolean {
-    return !!contentType && contentType.includes('text/plain');
+    return (
+        !!contentType &&
+        /^text\/.*|application\/(javascript|ecmascript|xml|html|x-www-form-urlencoded)/i.test(
+            contentType
+        )
+    );
 }
+
 export function isTextEventStream(contentType?: string): boolean {
     return !!contentType && contentType.includes('text/event-stream');
 }
