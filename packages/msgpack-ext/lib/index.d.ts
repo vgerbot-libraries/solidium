f2421a8d55a20f3bc2c3891a41ab9c510c8bb599
y1j2x34 <y1j2x34@qq.com>
2026-05-04 09:54:04 +0800

    chore: bump version to 1.0.1

--- packages/msgpack-ext/lib/index.d.ts
+++ packages/msgpack-ext/lib/index.d.ts
@@ -1,5 +1,5 @@
 declare function decode(buffer: ArrayLike<number> | BufferSource): unknown;
 
-declare function encode(input: unknown): Uint8Array<ArrayBufferLike>;
+declare function encode(input: unknown): Uint8Array<ArrayBuffer>;
 
 export { decode, encode };
